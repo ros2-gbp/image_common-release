8859cd94c9cc9fac960c7082a489b4ef13240877
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-11-24 15:42:10 +0100

    Imported upstream version '6.4.3' of 'upstream'

--- camera_info_manager/include/camera_info_manager/camera_info_manager.hpp
+++ camera_info_manager/include/camera_info_manager/camera_info_manager.hpp
@@ -181,6 +181,8 @@ using SetCameraInfo = sensor_msgs::srv::SetCameraInfo;
 class CameraInfoManager
 {
 public:
+  [[deprecated("Use CameraInfoManager(rclcpp::node_interfaces::NodeBaseInterface::SharedPtr, ...)"
+    " instead.")]]
   CAMERA_INFO_MANAGER_PUBLIC
   CameraInfoManager(
     rclcpp::Node * node,
@@ -188,11 +190,23 @@ public:
     const std::string & url = "",
     const std::string & ns = "~");
 
+  [[deprecated("Use CameraInfoManager(rclcpp::node_interfaces::NodeBaseInterface::SharedPtr, ...)"
+    " instead.")]]
   CAMERA_INFO_MANAGER_PUBLIC
   CameraInfoManager(
     rclcpp_lifecycle::LifecycleNode * node,
     const std::string & cname = "camera",
     const std::string & url = "",
+    const std::string & ns = "~");
+
+  [[deprecated("Use CameraInfoManager(..., rclcpp::QoS, ...) instead")]]
+  CAMERA_INFO_MANAGER_PUBLIC
+  CameraInfoManager(
+    rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base_interface,
+    rclcpp::node_interfaces::NodeServicesInterface::SharedPtr node_services_interface,
+    rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logger_interface,
+    const std::string & cname = "camera", const std::string & url = "",
+    rmw_qos_profile_t custom_qos = rmw_qos_profile_default,
     const std::string & ns = "~");
 
   CAMERA_INFO_MANAGER_PUBLIC
@@ -201,7 +215,7 @@ public:
     rclcpp::node_interfaces::NodeServicesInterface::SharedPtr node_services_interface,
     rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logger_interface,
     const std::string & cname = "camera", const std::string & url = "",
-    rmw_qos_profile_t custom_qos = rmw_qos_profile_default,
+    rclcpp::QoS custom_qos = rclcpp::SystemDefaultsQoS(),
     const std::string & ns = "~");
 
   CAMERA_INFO_MANAGER_PUBLIC
